977d2208bb8dde42995637e1ee54f2c622840908
irsyad0141 <1rsy4d.m@gmail.com>
2021-11-18 20:44:49 +0700

    Added Solution to Problem Four

--- Assets/Scripts/CircleMove.cs
+++ Assets/Scripts/CircleMove.cs
@@ -4,20 +4,33 @@ public class CircleMove : MonoBehaviour
 {
     Rigidbody2D rb;
 
-    private int xPos;
-    private int yPos;
-
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        xPos = Random.Range(-10, 10);
-        yPos = Random.Range(-10, 10);
+    }
 
+    private void Update()
+    {
         Move();
     }
 
     private void Move()
     {
-        rb.velocity = new Vector2(xPos, yPos);
+        if (Input.GetKey("up"))
+        {
+            rb.velocity = new Vector2(0, 10);
+        }
+        else if (Input.GetKey("down"))
+        {
+            rb.velocity = new Vector2(0, -10);
+        }
+        else if (Input.GetKey("left"))
+        {
+            rb.velocity = new Vector2(-10, 0);
+        }
+        else if (Input.GetKey("right"))
+        {
+            rb.velocity = new Vector2(10, 0);
+        }
     }
 }
