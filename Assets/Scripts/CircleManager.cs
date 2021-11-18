87b31c7304645270f163cd31d538431cdc823ff4
irsyad0141 <1rsy4d.m@gmail.com>
2021-11-18 19:54:31 +0700

    Initial commit

---
+++ Assets/Scripts/CircleManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleManager : MonoBehaviour
+{
+    public GameObject Circle;
+
+    private void Awake()
+    {
+        Instantiate(Circle, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
