9f9e529b61a99e42cc11ee6fac7ed75872b996e1
irsyad0141 <1rsy4d.m@gmail.com>
2021-11-18 20:09:55 +0700

    Added Script CircleMove

---
+++ Assets/Scripts/CircleMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CircleMove : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    private int xPos;
+    private int yPos;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        xPos = Random.Range(-10, 10);
+        yPos = Random.Range(-10, 10);
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector3(xPos, yPos, 0);
+    }
+}
