6f1fbd3e74042a24f50a19760b427f4b78df9fbd
irsyad0141 <1rsy4d.m@gmail.com>
2021-11-20 08:45:54 +0700

    Added Solution to Problem Six

---
+++ Assets/Scripts/GetSquareArea.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[DefaultExecutionOrder(-1)]
+public class GetSquareArea : MonoBehaviour
+{
+    public float left;
+    public float right;
+    public float up;
+    public float down;
+
+    private void Awake()
+    {
+        GetAroundLength();
+    }
+
+    void GetAroundLength()
+    {
+        left = -GetLength(Vector2.left);
+        right = GetLength(Vector2.right);
+        up = GetLength(Vector2.up);
+        down = -GetLength(Vector2.down);
+    }
+
+    float GetLength(Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction);
+        if (hit.collider != null)
+        {
+            return (hit.point - (Vector2)transform.position).magnitude;
+        }
+        return -1;
+    }
+}
---
+++ Assets/Scripts/RectangleManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class RectangleManager : MonoBehaviour
+{
+    public GameObject[] Rectangles;
+    public GetSquareArea _area;
+
+    private float randX;
+    private float randY;
+
+    private void Awake()
+    {
+        RandomizePosition();
+
+        for (int i = 0; i < Rectangles.Length; i++)
+        {
+            Instantiate(Rectangles[i], new Vector3(randX, randY, 0), Quaternion.identity);
+            RandomizePosition();
+        }
+    }
+
+    public void RandomizePosition()
+    {
+        randX = Random.Range(_area.left, _area.right);
+        randY = Random.Range(_area.down, _area.up);
+    }
+}
